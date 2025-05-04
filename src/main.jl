27b1ba2da7991ee285ced7684470670a71863188
tim21821 <tim.mattick@gmx.de>
2025-05-04 16:00:36 +0200

    Remove anagrams

--- src/main.jl
+++ src/main.jl
@@ -12,7 +12,14 @@ end
 
 filtervalidwords!(words::Vector{String}) = filter!(isvalidword, words)
 
+getbitrepresentation(c::Char) = one(UInt32) << (UInt8(c) - 0x61)
+
+getbitrepresentation(str::AbstractString)::UInt32 = sum(getbitrepresentation, str)
+
+removeanagrams!(words::Vector{String}) = unique!(getbitrepresentation, words)
+
 function main()
     words = readfromfile("words_alpha.txt")
     filtervalidwords!(words)
+    removeanagrams!(words)
 end
